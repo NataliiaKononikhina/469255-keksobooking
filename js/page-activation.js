aca1afd73e9c7251bad1addd9b92c9dd8c47aa10
Nataliia Kononikhina <kononikhinan@gmail.com>
2018-02-16 08:48:44 +0100

    Фикс кода

--- js/page-activation.js
+++ js/page-activation.js
@@ -1,12 +1,17 @@
 'use strict';
 
 (function () {
+  var LIMITATION_TOP = 100;
+  var LIMITATION_BOTTOM = 500;
+  var FROM_CENTER_TO_BOTTOM_PIN = 48;
+  // var FROM_CENTER_TO_LEFT_PIN = 65;
+
   var address = document.querySelector('#address');
   var mapPinMain = window.util.map.querySelector('.map__pin--main');
 
   // Добавление адреса в поле "Адрес"
   var setAddress = function (element) {
-    address.value = element.offsetLeft + ', ' + (element.offsetTop + 48);
+    address.value = element.offsetLeft + ', ' + (element.offsetTop + FROM_CENTER_TO_BOTTOM_PIN);
   };
 
   // Метод активации страницы
@@ -25,11 +30,38 @@
 
   setAddress(mapPinMain);
 
+  // mapPinMain.addEventListener('mousedown', function (evt) {
+  //   evt.preventDefault();
+
+  //   var onMouseMove = function (moveEvt) {
+  //     moveEvt.preventDefault();
+
+  //     mapPinMain.style.top = (moveEvt.clientY) + 'px';
+  //     mapPinMain.style.left = (moveEvt.clientX - FROM_CENTER_TO_LEFT_PIN) + 'px';
+
+  //     if ((moveEvt.clientY) > LIMITATION_BOTTOM) {
+  //       mapPinMain.style.top = LIMITATION_BOTTOM + 'px';
+  //     }
+  //     if ((moveEvt.clientY) < LIMITATION_TOP) {
+  //       mapPinMain.style.top = LIMITATION_TOP + 'px';
+  //     }
+  //   };
+
+  //   var onMouseUp = function (upEvt) {
+  //     upEvt.preventDefault();
+
+  //     window.util.map.removeEventListener('mousemove', onMouseMove);
+  //     window.util.map.removeEventListener('mouseup', onMouseUp);
+  //     mapPinMain.removeEventListener('mouseup', activate);
+  //   };
+
+  //   window.util.map.addEventListener('mousemove', onMouseMove);
+  //   window.util.map.addEventListener('mouseup', onMouseUp);
+  //   mapPinMain.addEventListener('mouseup', activate);
+  // });
+
   mapPinMain.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
-
-    var limitationTop = 100;
-    var limitationBottom = 500;
 
     var startCoords = {
       x: evt.clientX,
@@ -49,7 +81,7 @@
         y: moveEvt.clientY
       };
 
-      if (limitationTop < moveEvt.clientY && moveEvt.clientY < limitationBottom) {
+      if (moveEvt.clientY > LIMITATION_TOP && moveEvt.clientY < LIMITATION_BOTTOM) {
         mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
         mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
       }
@@ -60,6 +92,7 @@
 
       window.util.map.removeEventListener('mousemove', onMouseMove);
       window.util.map.removeEventListener('mouseup', onMouseUp);
+      mapPinMain.removeEventListener('mouseup', activate);
     };
 
     window.util.map.addEventListener('mousemove', onMouseMove);
