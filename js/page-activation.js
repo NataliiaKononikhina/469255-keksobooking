f6fc3c75cef1ce6817b88a56fbb5916ce9654aec
Nataliia Kononikhina <kononikhinan@gmail.com>
2018-02-17 13:06:36 +0100

    Ограничила передвижение пина

--- js/page-activation.js
+++ js/page-activation.js
@@ -2,10 +2,9 @@
 
 (function () {
   var LIMITATION_TOP = 100;
-  var LIMITATION_BOTTOM = 550;
-  var FROM_CENTER_TO_BOTTOM_PIN = 48;
+  var LIMITATION_BOTTOM = 500;
+  var FROM_CENTER_TO_BOTTOM_PIN = 45;
   var PIN_HEIGHT = 155;
-  // var FROM_CENTER_TO_LEFT_PIN = 65;
 
   var address = document.querySelector('#address');
   var mapPinMain = window.util.map.querySelector('.map__pin--main');
@@ -31,36 +30,6 @@
 
   setAddress(mapPinMain);
 
-  // mapPinMain.addEventListener('mousedown', function (evt) {
-  //   evt.preventDefault();
-
-  //   var onMouseMove = function (moveEvt) {
-  //     moveEvt.preventDefault();
-
-  //     mapPinMain.style.top = (moveEvt.clientY) + 'px';
-  //     mapPinMain.style.left = (moveEvt.clientX - FROM_CENTER_TO_LEFT_PIN) + 'px';
-
-  //     if ((moveEvt.clientY) > LIMITATION_BOTTOM) {
-  //       mapPinMain.style.top = LIMITATION_BOTTOM + 'px';
-  //     }
-  //     if ((moveEvt.clientY) < LIMITATION_TOP) {
-  //       mapPinMain.style.top = LIMITATION_TOP + 'px';
-  //     }
-  //   };
-
-  //   var onMouseUp = function (upEvt) {
-  //     upEvt.preventDefault();
-
-  //     window.util.map.removeEventListener('mousemove', onMouseMove);
-  //     window.util.map.removeEventListener('mouseup', onMouseUp);
-  //     mapPinMain.removeEventListener('mouseup', activate);
-  //   };
-
-  //   window.util.map.addEventListener('mousemove', onMouseMove);
-  //   window.util.map.addEventListener('mouseup', onMouseUp);
-  //   mapPinMain.addEventListener('mouseup', activate);
-  // });
-
   mapPinMain.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
 
@@ -85,10 +54,10 @@
       mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
       mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
 
-      if (moveEvt.clientY < LIMITATION_TOP) {
+      if (mapPinMain.offsetTop < LIMITATION_TOP) {
         mapPinMain.style.top = LIMITATION_TOP + 'px';
       }
-      if (moveEvt.clientY > LIMITATION_BOTTOM) {
+      if ((mapPinMain.offsetTop - PIN_HEIGHT) > LIMITATION_BOTTOM) {
         mapPinMain.style.top = (LIMITATION_BOTTOM + PIN_HEIGHT) + 'px';
       }
     };
@@ -96,13 +65,17 @@
     var onMouseUp = function (upEvt) {
       upEvt.preventDefault();
 
+      onMouseMove(upEvt);
+
       window.util.map.removeEventListener('mousemove', onMouseMove);
       window.util.map.removeEventListener('mouseup', onMouseUp);
+      window.util.map.removeEventListener('mouseleave', onMouseUp);
       mapPinMain.removeEventListener('mouseup', activate);
     };
 
     window.util.map.addEventListener('mousemove', onMouseMove);
     window.util.map.addEventListener('mouseup', onMouseUp);
+    window.util.map.addEventListener('mouseleave', onMouseUp);
     mapPinMain.addEventListener('mouseup', activate);
   });
 })();
