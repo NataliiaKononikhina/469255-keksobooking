856fe7bc4f75eb346c1a78ab715c62930c96d36c
Nataliia Kononikhina <kononikhinan@gmail.com>
2018-03-06 15:04:30 +0100

    Задала ширину фото

--- js/photo-upload.js
+++ js/photo-upload.js
@@ -2,6 +2,7 @@
 
 (function () {
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var PHOTO_PREVIEW_WIDTH = '200%';
 
   var avatarFileChooser = document.querySelector('#avatar');
   var avatarPreview = document.querySelector('.notice__preview img');
@@ -35,6 +36,7 @@
     var photoPreview = document.createElement('img');
     var formPhotoContainer = document.querySelector('.form__photo-container');
 
+    photoPreview.style.width = PHOTO_PREVIEW_WIDTH;
     formPhotoContainer.appendChild(photoPreview);
 
     photoUpload(photoFileChooser, photoPreview);
